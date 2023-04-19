40d28e67ffefca2f0f84eedd36ffc1c2e9ecd22b
Alexandre Fernandes <73003542+Shivverz@users.noreply.github.com>
2023-04-19 23:40:29 +0100

    tokens O.o

--- projeto_PL_tokenizer.py
+++ projeto_PL_tokenizer.py
@@ -6,6 +6,14 @@ tokens = (
     'BOOL',
     'NULL',
     'NUMERIC', # TODO: Partir isto em vários tipos de números (tipo REAL, INTEGER, SCIENTIFIC). Posso fazer tudo dentro da mesma func tho (pensar se vale a pena)
+    #'INTEGER',
+    #'FLOAT',
+    #'SCIENTIFIC_NOTATION',
+    #'BINARY',
+    #'OCTAL',
+    #'HEXA',
+    'NAN',
+    'INFINITY',
     'APAR',
     'CPAR',
     'APAR2',
@@ -14,10 +22,12 @@ tokens = (
     'DATE',
     'TIME',         # TODO: Ver documentação do toml (tenho de trocar umas coisas no último parâmetro do tempo, mas não é nada difícl de fazer)
     'DATETIME',
-    'STRING'        # TODO: Inclui suporte para NaN e infinity
+    'STRING',  
     #'MULTILINE_STRING,
-    #'LITERAL_STRING',              # TODO: Fazer 2 estados para conseguir retirar os tokens de strings multiline (ver exercícios dos comentários das fichas PL)
-    #'MULTILINE_LITERAL_STRING'
+    'STRING_LITERAL',              # TODO: Fazer 2 estados para conseguir retirar os tokens de strings multiline (ver exercícios dos comentários das fichas PL)
+    #'MULTILINE_STRING_LITERAL',
+    'ACHAV',
+    'CCHAV'
 )
 
 t_EQUALS = r'='
@@ -26,6 +36,8 @@ t_APAR = r'\['
 t_CPAR = r'\]'
 t_APAR2 = r'\[\['
 t_CPAR2 = r'\]\]'
+t_ACHAV = r'\{'
+t_CCHAV = r'\}'
 
 def t_COMMENT(t):
     r'\#.+'
@@ -39,25 +51,37 @@ def t_NULL(t):
     r'null'
     return t
 
+def t_NAN(t):
+    r'(\+|-)?nan'
+    return t
+
+def t_INFINITY(t):
+    r'(\+|-)?infinity'
+    return t
+
+def t_STRING_LITERAL(t):
+    r"'[\sa-zA-Z0-9_.-]+'"
+    return t
+
 def t_STRING(t):
-    r'\".+\"|[a-zA-Z_-][a-zA-Z_\-0-9]*(\.\".+\"|\.[a-zA-Z_0-9\-]+)*'
+    r'\"[\sa-zA-Z0-9_.-]+\"|[a-zA-Z_-][a-zA-Z_\-0-9]*(\.\".+\"|\.[a-zA-Z_0-9\-]+)*' # Podemos ter que rever está expressão regular!
     if t.value[0] != '\"':
         t.type = 'KEY'
     return t
 
-def t_ANY_DATETIME(t):
+def t_DATETIME(t):
     r'\d+-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(-\d{2}:\d{2}|\w+)?'
     return t
 
-def t_ANY_DATE(t):
+def t_DATE(t):
     r'\d+-\d{2}-\d{2}'
     return t
 
-def t_ANY_TIME(t):
+def t_TIME(t):
     r'\d{2}:\d{2}:\d{2}(-\d{2}:\d{2}|\w+)?'
     return t
 
-def t_ANY_NUMERIC(t):
+def t_NUMERIC(t):
     r'(\+|-)?\d+\.?\d*'
     return t
 
@@ -86,6 +110,7 @@ ports = [ 8000, 8001, 8002 ]
 connection_max = 5000
 enabled = true
 type = null
+sl = '  string literal'
 
 [servers]
 
@@ -93,6 +118,8 @@ type = null
   [servers."alpha"."delta"]
   ip = "10.0.0.1"
   dc = "eqdc10"
+  inf = +infinity
+  n = -nan
 
   [servers.beta]
   ip = "10.0.0.2"
@@ -106,6 +133,8 @@ hosts = [
   "alpha",
   "omega"
 ]
+
+inline = { test = "test", more = "more", name = null }
 
 [[fruit]]
 name = "potato"
